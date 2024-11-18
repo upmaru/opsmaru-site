aa2da98f7ca726d8350fed67e685a7d8340d6683
Zack Siri <me@zacksiri.dev>
2024-11-18 15:53:49 +0700

    Remove static testimonials

--- assets/components/slider.jsx
+++ assets/components/slider.jsx
@@ -12,50 +12,6 @@ import { useCallback, useLayoutEffect, useRef, useState } from "react";
 import useMeasure from "react-use-measure";
 import { Container } from "./container";
 
-// const testimonials = [
-//   {
-//     img: "/images/testimonials/tina-yards.jpg",
-//     name: "Tina Yards",
-//     title: "VP of Sales, Protocol",
-//     quote:
-//       "Thanks to Radiant, we’re finding new leads that we never would have found with legal methods.",
-//   },
-//   {
-//     img: "/images/testimonials/conor-neville.jpg",
-//     name: "Conor Neville",
-//     title: "Head of Customer Success, TaxPal",
-//     quote:
-//       "Radiant made undercutting all of our competitors an absolute breeze.",
-//   },
-//   {
-//     img: "/images/testimonials/amy-chase.jpg",
-//     name: "Amy Chase",
-//     title: "Head of GTM, Pocket",
-//     quote:
-//       "We closed a deal in literally a few minutes because we knew their exact budget.",
-//   },
-//   {
-//     img: "/images/testimonials/veronica-winton.jpg",
-//     name: "Veronica Winton",
-//     title: "CSO, Planeteria",
-//     quote:
-//       "We’ve managed to put two of our main competitors out of business in 6 months.",
-//   },
-//   {
-//     img: "/images/testimonials/dillon-lenora.jpg",
-//     name: "Dillon Lenora",
-//     title: "VP of Sales, Detax",
-//     quote: "I was able to replace 80% of my team with RadiantAI bots.",
-//   },
-//   {
-//     img: "/images/testimonials/harriet-arron.jpg",
-//     name: "Harriet Arron",
-//     title: "Account Manager, Commit",
-//     quote:
-//       "I’ve smashed all my targets without having to speak to a lead in months.",
-//   },
-// ];
-
 function TestimonialCard({
   name,
   title,
