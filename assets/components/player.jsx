f6f3a2c123db3a9e1ee544adccb0ae9fadfec8eb
Zack Siri <me@zacksiri.dev>
2024-11-21 00:01:34 +0700

    Setup episode live view

--- assets/components/player.jsx
+++ assets/components/player.jsx
@@ -3,6 +3,6 @@ import MuxPlayer from "@mux/mux-player-react"
 
 export function Player({ video, title }) {
   return (
-    <MuxPlayer playbackId={video.playback_id} accentColor="#67e8f9" metadata={{ video_title: title }} />
+    <MuxPlayer playbackId={video.playback_id} accentColor="#a78bfa" metadata={{ video_title: title }} />
   )
 }
--- lib/opsmaru_web/live/episode_live.ex
+++ lib/opsmaru_web/live/episode_live.ex
@@ -1,3 +1,16 @@
 defmodule OpsmaruWeb.EpisodeLive do
+  use OpsmaruWeb, :live_view
 
+  alias Opsmaru.Courses
+
+  def mount(%{"id" => episode_slug}, _session, socket) do
+    #episode = Courses.show_episode(episode_slug)
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div></div>
+    """
+  end
 end
