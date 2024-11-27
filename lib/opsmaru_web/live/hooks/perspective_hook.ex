0b7a8c967fc517b8b6850b86c44acb1694896c25
Zack Siri <me@zacksiri.dev>
2024-11-27 16:12:30 +0700

    Try io inspect

--- lib/opsmaru_web/live/hooks/perspective_hook.ex
+++ lib/opsmaru_web/live/hooks/perspective_hook.ex
@@ -5,7 +5,7 @@ defmodule OpsmaruWeb.PerspectiveHook do
   alias Opsmaru.Cache
 
   def on_mount(:default, _params, _session, socket) do
-    case get_connect_info(socket, :uri) do
+    case get_connect_info(socket, :uri) |> IO.inspect() do
       %URI{host: host} ->
         socket =
           if host =~ "preview" do
@@ -19,7 +19,7 @@ defmodule OpsmaruWeb.PerspectiveHook do
         {:cont, socket}
 
       nil ->
-        {:cont, assign(socket, :perspective, fn -> "published" end)}
+        {:cont, assign_new(socket, :perspective, fn -> "published" end)}
     end
   end
 end
