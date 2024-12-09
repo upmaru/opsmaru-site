5b55cd0d0c32714d4c3dde4903929d93cba9e695
Zack Siri <me@zacksiri.dev>
2024-12-09 18:58:39 +0700

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Opsmaru.MixProject do
   def project do
     [
       app: :opsmaru,
-      version: "0.1.12",
+      version: "0.1.13",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
